7201f5d12bb8ca7c98ad3312f29f89c9fe7a139d
psurge1 <suraj.sswamy@gmail.com>
2022-01-16 11:17:05 -0600

    added graph to embed

--- stockbot.py
+++ stockbot.py
@@ -1,5 +1,9 @@
+from tkinter import font
 import discord
 from discord.ext import commands, tasks
+
+from matplotlib import pyplot as plt
+import time
 
 from utils import file_manager, data_cleaner
 from utils.file_tools import csv_tools as csv_manipulator, txt_tools as txt_manipulator
@@ -8,7 +12,7 @@ prefix="$"
 
 client=commands.Bot(command_prefix=prefix)
 
-# client.remove_command('help')
+client.remove_command('help')
 
 r=1
 
@@ -27,28 +31,38 @@ dataset=csv_tool.read(r"data\nasdaq_screener_1640713211469.csv")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Stocks'))
-    update.start()
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Stocks | $help'))
+    # update.start()
     print (f"Bot is ready. \nDiscord.py {discord.__version__}")
-'''
+    print (time.strftime("%H:%M:%S", time.localtime()))
+
 @client.command(pass_context=True)
-async def help():
-    author = ctx.message.author
-'''
+async def help(ctx, cmd=""):
+    help_embed=discord.Embed(
+        title="Help",
+        description="Type ``$help [command]`` for more info on a command",
+        colour=discord.Colour.green()
+    )
+
+    help_embed.add_field(name="General", value=f"`` clear \n greet \n graph \n code ``", inline=True)
+    help_embed.add_field(name="General", value=f"`` clear \n greet \n graph \n code ``", inline=True)
+
+    await ctx.send(embed=help_embed)
+
 @tasks.loop(seconds=30)
 async def update():
     global r, list_value
     if r%2==0:
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status_list[list_value%2]))
-        if status_list[list_value%2] == "Bull":
-            await client.user.edit(avatar=pfp1)
-        elif status_list[list_value%2] == "Bear":
-            await client.user.edit(avatar=pfp2)
-        list_value+=1
+        # if status_list[list_value%2] == "Bull":
+        #     # await client.user.edit(avatar=pfp1)
+        # elif status_list[list_value%2] == "Bear":
+        #     await client.user.edit(avatar=pfp2)
+        # list_value+=1
     else:
         await client.change_presence(status=discord.Status.online, activity=discord.Game('Stocks'))
-        await client.user.edit(avatar=pfp1)
-    r+=1; """change"""
+        # await client.user.edit(avatar=pfp1)
+    r+=2; """change"""
 
 @client.command()
 async def clear(ctx, amount=1):
@@ -65,45 +79,64 @@ async def greet(ctx):
     print("DM")
 
 @client.command()
-async def check(ctx, stock_label):
+async def check(ctx, a, b=''):
     # embed with graph and data
-    uppercase_stock_label=stock_label.upper()
-    if (stock_label.upper() in dataset.keys()):
+    b=b.upper()
+    a=a.title()
+    if b in dataset.keys():
+        a="IPO Year" if a=="Ipo" else a
+        if a in dataset[b].keys():
+            detail_embed=discord.Embed(
+                title=b,
+                colour=discord.Colour.green()
+            )
+            detail_embed.add_field(name=a, value=dataset[b][a])
+            detail_embed.set_footer(text="Stock data provided by nasdaq.com")
+
+            await ctx.send(embed=detail_embed)
+
+        else:
+            await ctx.send(embed=discord.Embed(
+                description="Detail not found",
+                colour=discord.Colour.green()
+            ))
+    elif a.upper() in dataset.keys():
+        a=a.upper()
         stock_embed=discord.Embed(
-            title=uppercase_stock_label,
-            description=dataset[uppercase_stock_label]["Name"],
+            title=a,
+            description=dataset[a]['Name'],
             colour=discord.Colour.green()
         )
-        stock_embed.set_footer(text="Stock data provided by google.com and nasdaq.com")
-        # stock_embed.set_image(url="")
-        stock_embed.add_field(name="Source", value=f"https://www.google.com/search?q={stock_label.lower()}")
+        stock_embed.set_footer(text=f"Stock data provided by nasdaq.com")
+
+        n=1
+        for k in dataset[a]:
+            if n>1:
+                stock_embed.add_field(name=k, value=dataset[a][k], inline=True)
+            n+=1
         
-        await ctx.send(embed=stock_embed)
+        graph(a)
+        image = discord.File(r"img\graph.png", filename="graph.png")
+        stock_embed.set_image(url="attachment://graph.png")
+        
+        await ctx.send(file=image, embed=stock_embed)
+
     else:
-        await ctx.send("Invalid stock symbol")
+        await ctx.send(embed=discord.Embed(
+                description="Invalid stock symbol",
+                colour=discord.Colour.green()
+            ))
+
+# @client.command
+# async def error(ctx, text):
+#     await ctx.send(embed=discord.Embed(
+#                 description=text,
+#                 colour=discord.Colour.green()
+#             ))
 
 @client.command()
-async def embed(ctx):
-    stock_embed = discord.Embed(
-        title="Title",
-        description="Description",
-        colour=discord.Colour.blue()
-    )
-    stock_embed.set_footer(text="Footer")
-    # embed.set_image(url="")
-    # embed.set_thumbnail(url="")
-    stock_embed.set_author(name="author")
-    # embed.set_author(name="author", icon_url="")
-
-    stock_embed.add_field(name="Field Text", value="Field Value", inline=True)
-    stock_embed.add_field(name="Field Text", value="Field Value", inline=True)
-    stock_embed.add_field(name="Field Text", value="Field Value", inline=False)
-
-    await ctx.send(embed=stock_embed)
-
-@client.command()
-async def graph(ctx, stock_label):
-    await ctx.send(stock_label)
+async def define(ctx, *, arg):
+    return lambda x, y: x * y
 
 @client.command()
 async def code(ctx, *, arg):
@@ -134,6 +167,43 @@ async def code(ctx, *, arg):
 #         # delete
 #         print("todo: deleteApp command")
 
+def graph(stock_label, xtitle=None, ytitle=None):
+    dev_x_one=[25,26,27,28,29,30,31,32,33,34,35]
+    dev_y_one=[384, 420, 467, 493, 532, 560, 623, 649, 673, 687, 737]
+    dev_x_two=[]
+    dev_y_two=[]
+    for i in range(len(dev_x_one)):
+        dev_x_two.append(dev_x_one[i]+5)
+        dev_y_two.append(dev_y_one[i]*2)
 
+    plt.figure(facecolor='None')
+    
+    fig = plt.figure()
+    
+    ax = fig.add_subplot(111)
+
+    fig.patch.set_facecolor('None')
+
+    ax.plot(dev_x_one, dev_y_one, color='b', label='One')
+    ax.plot(dev_x_two, dev_y_two, color='r', label='Two')
+
+    # ax.set_title(stock_label, color='white', fontsize=20, fontweight='bold')
+    for a in (xtitle, ytitle):
+        if a!=None:
+            ax.set_ylabel(a, fontsize=12, fontweight='bold')
+
+    for b in ['bottom', 'top', 'left', 'right']:
+        ax.spines[b].set_color('white')
+    
+    ax.xaxis.label.set_color('white')
+    ax.yaxis.label.set_color('white')
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+    ax.set_facecolor('None')
+
+    ax.legend()
+
+    plt.savefig(r"img\graph.png", dpi=100)
+    plt.close()
 
 client.run(txt_tool.read('./token.txt'))
--- utils/web_capture.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-import subprocess
-subprocess.call(["python",r"C:\users\suraj\appdata\local\programs\python\python39\lib\site-packages\webkit2png-0.8.2-py3.9.egg-info","https://www.google.com/search?q=googl"])
